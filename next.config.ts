1f48808f8c5cf90d55a653fde07adb2b0a06cacb
홍요한 <ghddygks45@gmail.com>
2026-01-27 19:42:56 +0900

    feat: svgr 적용, /public/asset폴더에 아이콘 다운로드

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
